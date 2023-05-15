ad7a4506b63741d3562b7f7dcfb0497d3737118c
Ruslan Gaifullin <gayfullin.r.i@yandex.ru>
2023-05-15 20:15:19 +0300

    Initial Commit

---
+++ WBTestAvia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBTestAvia
+//
+//  Created by Руслан Гайфуллин on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
